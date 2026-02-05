b352a1359c06979b24b07a77674904061e1ea237
Hossam <hossammostafa203203@gmail.com>
2026-02-05 17:50:59 +0200

    #Question 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Question 1
+
+            
+            double d = 9.99;
+            int x = (int)d;
+            Console.WriteLine(x);
+            
+            Console.WriteLine("the variable d is a double with value 9.99 " +
+                "in C#, casting from double to int truncates the decimal part " +
+                "So 9.99 becomes 9, not 10.");
+            #endregion
+
+
+        }
+    }
+}
